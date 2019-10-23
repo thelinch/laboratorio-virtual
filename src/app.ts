469784318b0beb1a48fc16ad5b57081ae9e8a28c
kerberos <kerberos.team.2019@gmail.com>
2019-10-23 12:00:39 -0500

    scafold

--- src/app.ts
+++ src/app.ts
@@ -12,8 +12,7 @@ class App {
   }
 
   private config(): void {
-    
-     this.app.use(cors());
+    this.app.use(cors());
     // support application/json type post data
     this.app.use(bodyParser.json());
     // support application/x-www-form-urlencoded post data
@@ -23,7 +22,7 @@ class App {
       })
     );
 
-    this.app.use("/api/v1", routes);
+    this.app.use(routes);
   }
 }
 
